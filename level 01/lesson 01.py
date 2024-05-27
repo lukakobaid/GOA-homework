7088417a9b151ef5aa0a78845ea9fe5b8dd5981c
lukakobaid <lukagiorgikobaidze3@Gmail.com>
2024-05-27 12:44:48 +0400

    first commit

---
+++ level 01/lesson 01.py
@@ -0,0 +1,15 @@
+name = "luka" #str (string)(text)
+surname = "kobaidze"
+age = 11 #int (integer)(ricxvi)
+height = 1.53 # float (atwiladi) 
+
+knows_programing = True  #boolean (bool) #true or false 
+is_ugly = False #true or false 
+
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programing))
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(knows_programing) + " " + str(is_ugly) )
